a9cc17f6480c5c65843855e33984bed7db0458a3
Koda <85606515+koda-git@users.noreply.github.com>
2024-03-11 12:15:26 -0400

    Parser with nom test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use nom::{character::complete::alphanumeric1, IResult};
+
+fn parse_alphanumeric(input: &str) -> IResult<&str, &str> {
+    alphanumeric1(input)
 }
+
+fn main() {
+    let result = parse_alphanumeric("Hello123");
+    println!("{:?}", result)
+}
+
+
